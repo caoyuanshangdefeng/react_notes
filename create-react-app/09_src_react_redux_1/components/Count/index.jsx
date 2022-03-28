02c2f67ee22a6e3673de9a27f898cc728079ad41
caoyuanshangdefeng <18292676557@139.com>
2022-03-29 07:05:50 +0800

    react redux 纯react版

---
+++ create-react-app/09_src_react_redux_1/components/Count/index.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+    state = {
+        count: 0
+    }
+    // 加法
+    increment = () => {
+        const {value}=this.selectNumber
+        const {count}=this.state
+        this.setState({count:count+value*1})
+
+
+    }
+    // 减法
+    decrement = () => {
+        const {value}=this.selectNumber
+        const {count}=this.state
+        this.setState({count:count-value*1})
+
+    }
+    // 奇数加
+    incrementIfOdd = () => {
+        const {value}=this.selectNumber
+        const {count}=this.state
+        if (count%2!==0){
+            this.setState({count:count+value*1})
+        }
+       
+
+    }
+    // 异步加
+    incrementAsyc = () => {
+        const {value}=this.selectNumber
+        const {count}=this.state
+        setTimeout(() => {
+            this.setState({count:count+value*1})
+
+            
+        }, 3000);
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>当前求和为: {this.state.count}</h1>
+                <select ref={c => this.selectNumber = c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>奇数+</button>&nbsp;
+                <button onClick={this.incrementAsyc}>异步+</button>
+            </div>
+        )
+    }
+}
