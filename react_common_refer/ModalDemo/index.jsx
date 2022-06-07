b6c216f8e127c748d46c4f68049e039617f871a0
caoyuanshangdefeng <18292676557@139.com>
2022-06-08 07:04:58 +0800

    modal css

--- react_common_refer/ModalDemo/index.jsx
+++ react_common_refer/ModalDemo/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './modal.css'
 export default function ModalDemo(props) {
   // const propTypes = {
   //   visible: PropTypes.bool,
@@ -8,43 +8,60 @@ export default function ModalDemo(props) {
   //   onCancel: PropTypes.func,
   //   children: PropTypes.element
   // }
-  const defaultProps = {
-    visible: false,
-    title: '标题',
-    onOk: () => { },
-    onCancel: () => { }
-  }
+
   const {
     visible,
     title,
-    children,
+    content,
     onOk,
     onCancel
   } = props
+  console.log('@@@', props);
 
 
-  const show = { zIndex: 2000, opacity: 1 }
-  const hide = { zIndex: -1, opacity: 0 }
-  const contShow = { width: '600px', height: '600px' }
-  const contHide = { width: '0px', height: '0px' }
+  // const show = { zIndex: 2000, opacity: 1 }
+  // const hide = { zIndex: -1, opacity: 0 }
+  // const contShow = { width: '600px', height: '600px' }
+  // const contHide = { width: '0px', height: '0px' }
 
 
 
 
 
+  // return (
+  //   <div className="gy-modalContainer" style={ visible ? show : hide }>
+  //     <div className="mask" onClick={onCancel}></div>
+  //     <div className="innerContent" style={ visible ? contShow : contHide }>
+  //       <div className="innerContent-header">
+  //         <div className="innerContent-title">{title}</div>
+  //       </div>
+  //       <div className="innerContent-center">
+  //         children
+  //         {/* {content} */}
+  //         {JSON.stringify(content)}
+  //       </div>
+  //       <div className="innerContent-footer">
+  //         <button type='cancel' onClick={onCancel}>取消</button>
+  //         <button type='primary' onClick={onOk}>确定</button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
   return (
-    <div className="gy-modalContainer" style={ visible ? show : hide }>
-      <div className="mask" onClick={onCancel}></div>
-      <div className="innerContent" style={ visible ? contShow : contHide }>
-        <div className="innerContent-header">
-          <div className="innerContent-title">{title}</div>
+    <div className="modal-mask" onClick={onCancel}>
+      <div className="modal-container" >
+
+        <div className="modal-header">
+          <div className="modal-title">{title}</div>
         </div>
-        <div className="innerContent-center">
-          {children}
+        <div className="modal-body">
+          children
+          {/* {content} */}
+          {JSON.stringify(content)}
         </div>
-        <div className="innerContent-footer">
-          <button type='cancel' onClick={onCancel}>取消</button>
-          <button type='primary' onClick={onOk}>确定</button>
+        <div className="modal-footer">
+          <button className='modal-cancel-btn btn' onClick={onCancel}>取消</button>
+          <button className='modal-confirm-btn btn' onClick={onOk}>确定</button>
         </div>
       </div>
     </div>
